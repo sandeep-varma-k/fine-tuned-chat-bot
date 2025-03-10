6b24d87656a6b5a6b1da435f1b67143203d967c1
sandeep-varma-k <sandeepvarmak7@gmail.com>
2025-03-09 22:36:57 -0400

    Added pdf reader

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,24 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 
 
 st.header("Fine Tuned Chatbot")
 
+# Document Upload Section
 with st.sidebar:
     st.title("Document Store")
     file = st.file_uploader(" Upload a PDF file(s) to fine tune your chatbot", type="pdf")
+
+
+# Extract the text from pdf file
+if file is not None:
+    pdf_reader = PdfReader(file)
+    extracted_text = ""
+
+    for page in pdf_reader.pages:
+        extracted_text += page.extract_text()
+
+    # Print all text to screen
+    # st.write(extracted_text)
+
+
