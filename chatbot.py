f88c2c0809654954912e9a205d83cedaf6acb26d
sandeep-varma-k <sandeepvarmak7@gmail.com>
2025-04-18 16:51:30 -0400

    Q & A done

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,21 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.chat_models import ChatOpenAI
+
+from dotenv import load_dotenv
+import os
+
+
+# Load the API key from environment variables
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY is not set in the environment variables.")
 
 
 st.header("Fine Tuned Chatbot")
@@ -18,7 +34,38 @@ if file is not None:
     for page in pdf_reader.pages:
         extracted_text += page.extract_text()
 
-    # Print all text to screen
-    # st.write(extracted_text)
+    # Chunk the text into smaller pieces
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(extracted_text)
 
+    # Generate embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
+    # creating vector store - FAISS
+    vector_store = FAISS.from_texts(chunks, embeddings)
+
+    # get user question
+    user_question = st.text_input("Type Your question here")
+
+    # do similarity search
+    if user_question:
+        match = vector_store.similarity_search(user_question)
+
+        # define the LLM
+        llm = ChatOpenAI(
+            openai_api_key=OPENAI_API_KEY,
+            temperature=0,
+            max_tokens=1000,
+            model_name="gpt-3.5-turbo"
+        )
+
+        # output results
+        # chain -> take the question, get relevant document, pass it to the LLM, generate the output
+        chain = load_qa_chain(llm, chain_type="stuff")
+        response = chain.run(input_documents=match, question=user_question)
+        st.write(response)
